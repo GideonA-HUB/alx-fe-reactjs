edacd80a7d83aafaa1c054d27747cd13cefceaac
Gideon Amienghemhen <amienz0958@gmail.com>
2025-09-08 04:14:09 +0100

    Task 1 commit

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 
 function App() {
   return (
@@ -11,6 +12,10 @@ function App() {
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <UserProfile name="John" age="30" bio="Enjoys coding and playing guitar" />
+
+      {/* ✅ Counter Component */}
+      <Counter />
+
       <Footer />
     </>
   )

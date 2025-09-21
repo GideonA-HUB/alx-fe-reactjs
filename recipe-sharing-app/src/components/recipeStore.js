2d716daac7d8911c2ef6e3746c56de460d9b10c1
Gideon Amienghemhen <amienz0958@gmail.com>
2025-09-21 17:10:59 +0100

    Correction for task 2

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,13 +2,22 @@ import { create } from 'zustand';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
+  searchTerm: '',
+
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+
   setRecipes: (recipes) => set({ recipes }),
+
   updateRecipe: (updated) =>
     set((state) => ({
-      recipes: state.recipes.map((r) => (r.id === updated.id ? { ...r, ...updated } : r)),
+      recipes: state.recipes.map((r) =>
+        r.id === updated.id ? { ...r, ...updated } : r
+      ),
     })),
+
   deleteRecipe: (id) =>
     set((state) => ({ recipes: state.recipes.filter((r) => r.id !== id) })),
+
+  setSearchTerm: (term) => set({ searchTerm: term }),
 }));
